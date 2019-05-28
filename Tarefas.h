04fcd9e99b0c8539d93faaef11912493f886334a
André <andrelagerocha@gmail.com>
2019-05-28 19:06:04 -0300

    Initial comit

---
+++ Tarefas.h
@@ -0,0 +1,21 @@
+#ifndef __TAREFAS_TP3_ROB__
+#define __TAREFAS_TP3_ROB__
+
+
+namespace Tarefas {
+
+	AlinhaComLampada();
+	
+	PercorreLinha();
+	
+	PercorreQuadrado();
+	
+	PercorreTriangulo();
+	
+	ExploraAmbiente();
+	
+}
+
+
+
+#endif
