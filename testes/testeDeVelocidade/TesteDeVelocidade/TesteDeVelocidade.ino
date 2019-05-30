79f143aefab2a8d24d8a579b8915842fdfbed006
André <andrelagerocha@gmail.com>
2019-05-30 20:15:42 -0300

    test

---
+++ testes/testeDeVelocidade/TesteDeVelocidade/TesteDeVelocidade.ino
@@ -0,0 +1,11 @@
+#include "/home/andre/Arduino/BatatiteColorite_TP3/src/BatatiteColorite/Movimentacao.h"
+#include "/home/andre/Arduino/BatatiteColorite_TP3/src/BatatiteColorite/Configuracao.h"
+
+
+void setup(){
+  
+}
+
+void loop(){
+  anda(10);
+}
