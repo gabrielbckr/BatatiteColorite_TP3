ffc623f7f424af7d03d75de9dd01c75644aea7ab
gabriel <gabriel.becker.59@gmail.com>
2019-05-30 22:11:51 -0300

    update encoderand inserted note about interrupt

--- src/BatatiteColorite/Encoder.h
+++ src/BatatiteColorite/Encoder.h
@@ -1,7 +1,19 @@
 #ifndef __ENCODER__TP__ROBOTICA__
 #define __ENCODER__TP__ROBOTICA__
 
+/*
 
+https://forum.arduino.cc/index.php?topic=41713.msg303026#msg303026
+
+Class member functions that appear to take no arguments actually 
+do take an invisible argument called this.  
+The value of this is the instance of the object you're referring to.  
+You can't attachInterrupt() to a class member function directly.  
+You might be able to do it if the member function is static, 
+but my approach is to have a global variable that is the instance, 
+and the global service routine knows how to use the global variable.
+
+*/
 class Encoder
 {
 	public:
@@ -17,7 +29,7 @@ class Encoder
 
 	float RetornaRPM();
 
-	void  ZeraVoltas();
+	void  ZeraVoltas()  { Voltas = 0; } 
 };
 
 
