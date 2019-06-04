9c78cfb1f6c6407f9d206b8bbba6dca36ba4bc34
gabriel <gabriel.becker.59@gmail.com>
2019-06-04 08:34:51 -0300

    Alteracoes para testes de seguidor de linha

--- src/BatatiteColorite/BatatiteColorite.ino
+++ src/BatatiteColorite/BatatiteColorite.ino
@@ -3,6 +3,7 @@
 #include "Encoder.h"
 #include <LiquidCrystal.h>
 #include "Arduino.h"
+#include "Tarefas.h"
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -34,144 +35,9 @@ void setup(){
   LEFT_MOTOR->setSpeed(DEFAULT_LEFT__VOLT_SPEED);
   RIGHT_MOTOR->setSpeed(DEFAULT_LEFT__VOLT_SPEED);
 
-
-  TestePH2();
+  Tarefas::ExploraAmbiente();
+  //Tarefas::ProcuraMaximo();
 } 
-
-void testeDeControle( )
-{
-  controladorMotorDireita.Tunning(0.5, 0, 20);
-  RIGHT_MOTOR->run(RIGHT_MOTOR_BACKWARD);
-  long int now = millis();
-
-  int rightSpeed = 5;
-
-  while(millis() - now < 8000){
-  ////////////////////////////////////////////////////////////////////// int erroEsquerda = leftSpeed - encoderMotorDireita.RetornaRPM(); 
-    double vel = encoderMotorDireita.RetornaRPM() ;
-    double erroDireita = rightSpeed - vel;
-  ////////////////////////////////////////////////////////////////////// int leftVelocityOutput  = controladorMotorEsquerda.LeiDeControle(erroEsquerda);
-    int rightVelocityOutput = controladorMotorDireita.LeiDeControle(erroDireita);
-    if (rightVelocityOutput > 0){
-      rightVelocityOutput = -1 * rightVelocityOutput;
-      RIGHT_MOTOR->run(RIGHT_MOTOR_FORWARD);
-    }
-    rightVelocityOutput = rightVelocityConstrainting(rightVelocityOutput);
-  ////////////////////////////////////////////////////////////////////// LEFT_MOTOR->setSpeed(leftVelocityConstrainting(leftVelocityOutput));
-    RIGHT_MOTOR->setSpeed(rightVelocityOutput);
-    lcd.setCursor(0, 0);
-    lcd.print(String(vel)+ "   " +String(rightVelocityOutput));
-    lcd.setCursor(0, 1);
-    lcd.print(erroDireita);
-    delay(300);
-  }
-  para();
-}
-
-void controlaRPM(double rightSpeed, double leftSpeed){
-
-  RIGHT_MOTOR->run(RIGHT_MOTOR_BACKWARD);
-  LEFT_MOTOR->run(LEFT_MOTOR_BACKWARD);
-  //long int now = millis();
-  
-  //while(millis() - now < 400){
-    double velDir = encoderMotorDireita.RetornaRPM() ;
-    double velEsq = encoderMotorEsquerda.RetornaRPM() ;
-    double erroDireita = rightSpeed - velDir;
-    double erroEsquerda = leftSpeed - velEsq;
-    float Kp = 4;
-    Serial.println(leftSpeed); 
-    Serial.print("leftSpeed");
-    Serial.println(velEsq); 
-    Serial.print("velocity antes eh");
-    Serial.println(leftVelocityOutput);
-    Serial.print("o erro eh");
-    Serial.println(erroEsquerda);
-    rightVelocityOutput = rightVelocityOutput +(erroDireita* Kp);
-
-    leftVelocityOutput =  leftVelocityOutput + (erroEsquerda* Kp);
-    
-    /*
-    if (erroDireita > 0.0){
-      if(erroDireita > rightSpeed*0.1){
-        rightVelocityOutput=rightVelocityOutput+10;
-      }
-      rightVelocityOutput=rightVelocityOutput+(erroDireita* Kp);
-    } else {
-  
-      if(erroDireita*(-1) > rightSpeed*0.1){
-        rightVelocityOutput=rightVelocityOutput-10;
-      }
-      rightVelocityOutput=rightVelocityOutput+(erroDireita* Kp);
-    }
-
-    if (erroEsquerda > 0.0){
-      if(erroEsquerda > leftSpeed*0.1){
-        leftVelocityOutput=leftVelocityOutput+10;
-      }
-      leftVelocityOutput=leftVelocityOutput+(erroEsquerda* Kp);
-    } else {
-      if(erroEsquerda*(-1) > leftSpeed*0.1){
-        leftVelocityOutput=leftVelocityOutput-10;
-      }
-      leftVelocityOutput=leftVelocityOutput+(erroEsquerda* Kp);
-    }
-    */
-    Serial.print("velocity eh");
-    Serial.println(leftVelocityOutput);
-    RIGHT_MOTOR->setSpeed(rightVelocityOutput);
-    LEFT_MOTOR->setSpeed(leftVelocityOutput);
-   
- // }
-delay(500);
-}
-
-void TestePH2(){
-
-  int blwhEsq = analogRead(12);
-  int blwhDir = analogRead(13);
-  
-  para();
-  RIGHT_MOTOR->run(RIGHT_MOTOR_BACKWARD);
-  LEFT_MOTOR->run(LEFT_MOTOR_BACKWARD);
-  
-  long int now = millis();
-
-
-  int limite = 400;
-  double velGeral = 3;
-
-  while(millis() - now < 15000){
-    int blwhEsq = analogRead(12);
-    int blwhDir = analogRead(13);
-
-    if (blwhEsq < limite && blwhDir < limite){
-      controlaRPM(velGeral, velGeral);
-    }else if(blwhEsq > limite && blwhDir < limite){
-      controlaRPM(velGeral,velGeral*0.7);
-    }else if(blwhEsq < limite && blwhDir > limite){
-      controlaRPM(velGeral*0.7 ,velGeral);
-    }else{
-      controlaRPM(velGeral,-velGeral);   
-  }
-    lcd.setCursor(0, 0);
-  
-  lcd.setCursor(0, 0);
-  lcd.print("       ");
-  lcd.setCursor(0, 0);
-  lcd.print(blwhEsq);
-  lcd.setCursor(0, 1);
-  lcd.print("       ");
-  lcd.setCursor(0, 1);
-  lcd.print(blwhDir);
-  delay(10);
-  }
-  
-  lcd.setCursor(5, 0);
-  lcd.print("ACABOU");
-  para();
-  
-}
 
 void loop()
 {
--- src/BatatiteColorite/SeguidorDeLinha.cpp
+++ src/BatatiteColorite/SeguidorDeLinha.cpp
@@ -1,26 +1,24 @@
 #include "SeguidorDeLinha.h"
 #include "Movimentacao.h"
 
-using namespace SeguidorDeLinha;
 
-void Seguir (int esquerda, int direita){
+void SeguidorDeLinha::Seguir (int esquerda, int direita){
 
-	anda();
 	if(esquerda > LIMIAR_LINHA && direita > LIMIAR_LINHA){
 
-		setControledRPMSpeed(LEFT_RPM, RIGHT_RPM); //SE OS DOIS ESTIVEREM DENTRO DA LINHA, O ROBO ANDARÁ PARA FRENTE
+		set_speed(DEFAULT_LEFT__VOLT_SPEED, DEFAULT_RIGHT_VOLT_SPEED); //SE OS DOIS ESTIVEREM DENTRO DA LINHA, O ROBO ANDARÁ PARA FRENTE
 
 	}else if(esquerda < LIMIAR_LINHA && direita > LIMIAR_LINHA){ // SE O LADO ESQUERDO ESTIVER FORA, O ROBO FARÁ UMA CURVA PARA DIREITA
 
-		setControledRPMSpeed(LEFT_RPM, RIGHT_RPM*TURNING_FACTOR);
+		setControledRPMSpeed(DEFAULT_LEFT__VOLT_SPEED, DEFAULT_RIGHT_VOLT_SPEED*TURNING_FACTOR);
 
 	}else if (esquerda > LIMIAR_LINHA && direita < LIMIAR_LINHA){ //ANALOGO AO ANTERIOR, 
 
-		setControledRPMSpeed(LEFT_RPM*TURNING_FACTOR, RIGHT_RPM);
+		setControledRPMSpeed(DEFAULT_LEFT__VOLT_SPEED*TURNING_FACTOR, DEFAULT_RIGHT_VOLT_SPEED);
 
 	}else { //SE OS 2 LADOS ESTIVEREM FORA DA LINHA O ROBO GIRA NO PRÓPRIO EIXO PROCURANDO UMA LINHA
 
-		setControledRPMSpeed(LEFT_RPM, -RIGHT_RPM);
+		setControledRPMSpeed(DEFAULT_LEFT__VOLT_SPEED, -DEFAULT_RIGHT_VOLT_SPEED);
 
 	}
 
